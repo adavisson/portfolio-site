a5b03265ae87f07cdcb629ab57afcac19b788f18
Andrew Davisson <akdavisson4@gmail.com>
2021-06-14 20:13:49 -0600

    add profile image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import '../styles/styles.scss';
-import Button from '../components/Button/Button';
+import Image from '../components/Image/Image';
+import profilePic from '../images/profile.jpg';
 
 const IndexPage: React.FC = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Home</h1>
-      <Button />
+      <Image imgSrc={profilePic} />
     </main>
   );
 };
